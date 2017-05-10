2f9dd10fbd4b455e069aa05e98986b1601e6d22e
rufuspereira <rafael.pereira.estal@ine.es>
2017-05-10 11:34:06 +0200

    Contiene lo relevante. Bibliografía bien.

--- stem.py
+++
@@ -1,5 +0,0 @@
-from nltk.stem import SnowballStemmer
-stemmer = SnowballStemmer('spanish')
-word=['ataque','atacar','ataco','atacante','atacado','ataqué']
-StemContent = [stemmer.stem(word).upper() for word in word]
-print(StemContent)
